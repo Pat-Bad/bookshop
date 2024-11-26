6f3c6c0b6a7b3e715c3ab27c790449ca250e85bc
Patricia <patricia.badji89@gmail.com>
2024-11-26 17:05:29 +0100

    changes alert

--- src/components/MyAlert.jsx
+++ src/components/MyAlert.jsx
@@ -1,11 +1,25 @@
+import { useState } from 'react';
 import Alert from 'react-bootstrap/Alert';
+import Button from 'react-bootstrap/Button';
 
-function MyAlert(props) {
+function MyAlert() {
+  const [show, setShow] = useState(true);
+
   return (
-   
-        <Alert className="bg-info">
-         Welcome, {props.name}!
-        </Alert>
-      )}
+    <>
+      <Alert show={show} variant="success">
+        <Alert.Heading>Benvenut*!</Alert.Heading>
+        
+        <div className="d-flex justify-content-end">
+          <Button onClick={() => setShow(false)} variant="outline-success">
+            Close me
+          </Button>
+        </div>
+      </Alert>
+
+      {!show && <Button onClick={() => setShow(true)}>Show Alert</Button>}
+    </>
+  );
+}
 
 export default MyAlert;
