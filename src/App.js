86eeb91d26c1332bc36ebf84debe91f10dc2c7da
Patricia <patricia.badji89@gmail.com>
2024-11-27 17:01:42 +0100

    corrections

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
 				<CustomNavBar />
 			</header>
 			<main>
-				<MyAlert name="Stefano" />
+				<MyAlert />
 				<Container fluid>
 					<Row>
 						<BookList />
--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -4,6 +4,7 @@ import { Col } from 'react-bootstrap';
 
 const BookList = () => {
   return (
+   
     <Col xs={12} md={4} lg={3}>
       {books.map((book, asin) => (
         <SingleBook key={asin} title={book.title} img={book.img} />
